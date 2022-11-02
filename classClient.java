fff11833a8e463b3681fffb4233f89eb52ddb518
biaggio <biaggiogzdev@gmail.com>
2022-11-02 14:18:06 +0100

    new class client

---
+++ classClient.java
@@ -0,0 +1,23 @@
+	public class Client{
+
+	String name;
+	String surname;
+	int age;
+	Float money;
+
+	Client( String name,
+        String surname,
+        int age,
+        Float money){
+
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+		this.money = money;
+
+}
+
+
+
+
+}
